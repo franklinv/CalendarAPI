33e07dbe8e7fb740f750b943052ded1e05fd781a
franklinv <franklin.vincent@gmail.com>
2020-03-31 13:54:26 +1100

    Add files via upload

---
+++ Models/CalendarDBContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoreWebAPIApp.Models
+{
+    public partial class CalendarDBContext : DbContext
+    {
+        public CalendarDBContext()
+        {
+        }
+
+        public CalendarDBContext(DbContextOptions<CalendarDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblEvent> TblEvent { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TblEvent>(entity =>
+            {
+                entity.ToTable("tblEvent");
+
+                entity.Property(e => e.EventLocation)
+                    .IsRequired()
+                    .HasMaxLength(80);
+
+                entity.Property(e => e.EventMembers).IsRequired();
+
+                entity.Property(e => e.EventName)
+                    .IsRequired()
+                    .HasMaxLength(80);
+
+                entity.Property(e => e.EventOrganizer)
+                    .IsRequired()
+                    .HasMaxLength(80);
+
+                entity.Property(e => e.EventTime).HasColumnType("datetime");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/TblEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreWebAPIApp.Models
+{
+    public partial class TblEvent
+    {
+        public int Id { get; set; }
+        public string EventName { get; set; }
+        public DateTime EventTime { get; set; }
+        public string EventLocation { get; set; }
+        public string EventMembers { get; set; }
+        public string EventOrganizer { get; set; }
+    }
+}
